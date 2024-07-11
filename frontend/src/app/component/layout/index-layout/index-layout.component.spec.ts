c6f9d1d3e1bc96193a336ff7d5a3de0aa8529870
Rednax0516 <alexanderjrcalawag@gmail.com>
2024-07-12 00:26:12 +0800

    added the tailwind

--- frontend/src/app/component/layout/index-layout/index-layout.component.spec.ts
+++ frontend/src/app/component/layout/index-layout/index-layout.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { IndexComponent } from './index.component';
+import { IndexLayoutComponent } from './index-layout.component';
 
-describe('IndexComponent', () => {
-  let component: IndexComponent;
-  let fixture: ComponentFixture<IndexComponent>;
+describe('IndexLayoutComponent', () => {
+  let component: IndexLayoutComponent;
+  let fixture: ComponentFixture<IndexLayoutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [IndexComponent]
+      imports: [IndexLayoutComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(IndexComponent);
+    fixture = TestBed.createComponent(IndexLayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ frontend/src/app/component/layout/index-layout/index-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-layout.component.html',
+  styleUrl: './index-layout.component.css'
+})
+export class IndexLayoutComponent {
+
+}
